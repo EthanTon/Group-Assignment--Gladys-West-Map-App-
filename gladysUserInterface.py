568af436b592e01c9a15bbaaecf1ed885373a074
Ethan Ton <ethan.m.ton@gmail.com>
2022-03-31 21:13:07 -0700

    Reworked login system

--- gladysUserInterface.py
+++ gladysUserInterface.py
@@ -1,1 +1,15 @@
-#Login Interface for Gladys
+# import gladysUserLogin
+# Login Interface for Gladys
+def ask_for_email():
+     """
+     Ask for an email
+     """
+     email = input("Enter your email: ")
+     return email
+
+def ask_for_password():
+     """
+     Ask for a password
+     """
+     password = input("Enter your password: ")
+     return password
--- gladysUserLogin.py
+++ gladysUserLogin.py
@@ -1,59 +1,94 @@
+import gladysUserInterface
 
 # Create a new user
+
+
 def check_email(username):
     """
     Check if the email is valid
     """
-    if "@" in username or "." in username:
+    if "@" in username and "." in username and not search_for_user(username):
         return True
     else:
         return False
 
-def ask_for_email():
+
+def set_email():
     """
-    Ask for an email
+    Sets an email
     """
-    email = input("Enter your email: ")
-    # print (email)
+    email = gladysUserInterface.ask_for_email()
     if check_email(email) == False:
         print("Invalid email")
-        email = ask_for_email()
+        email = set_email()
     return email
 
-def ask_for_password():
-    """
-    Ask for a password
-    """
-    password = input("Enter your password: ")
-    return password
 
 def set_password():
     """
     Sets a password
     """
-    password = input("Enter your password: ")
+    password = gladysUserInterface.ask_for_password()
     if len(password) < 1:
         print("Password must be at least 1 character long")
         password = set_password()
     return password
 
+
+def search_for_user(username):
+    """
+    Search for a user
+    """
+    user_file = open("users.txt", "r")
+    for line in user_file:
+        user_file_line = line.split(",")
+        # print(user_file_line)
+        if username == user_file_line[0]:
+            return True
+    return False
+
+
 def create_user():
     """
     Create a new user
     """
-    email = ask_for_email()
+    email = set_email()
     password = set_password()
 
     user_file = open("users.txt", "a")
-    user_file.write(email + "," + password + "\n")
-
+    user_file.write(email + "," + password + "," + "\n")
+    user_file.close()
 
 
 def login():
-    
+    """
+    Login
+    """
+    print("Type \" break \" to go back to login menu")
+
+    email = gladysUserInterface.ask_for_email()
+    password = gladysUserInterface.ask_for_password()
+    if email == "break":
+        return "back"
+    elif search_for_user(email) == False:
+        print("User not found! Make sure you have an account")
+        login()
+    elif email == "break":
+        return "back"
+    else:
+        user_file = open("users.txt", "r")
+        for line in user_file:
+            print(user_file)
+            user_file = line.split(",")
+            print(user_file)
+            if email == user_file[0] and password == user_file[1]:
+                print("Login successful")
+                user_file.close()
+                return email
+        print("Wrong password")
+        login()
 
 
-# create_user()  
-    
+# create_user()
 
-
+print(login())
